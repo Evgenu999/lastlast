0ab07beee7144bb59bb3124b78d37429a1aa2ca6
Evgenuu Sakin <shve-ster@yandex.ru>
2022-09-13 11:55:24 +0300

    example_giri

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 2;
+int b = 4;
+int c = 9;
+int d = 5;
+int e = 3;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
